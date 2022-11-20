a263d944eff5955aa97dbe6feff953aa08cfd49b
tayyab <mtayyabm222@gmail.com>
2022-11-20 12:28:32 +0500

    footer

--- src/components/Footer/FooterItems.jsx
+++ src/components/Footer/FooterItems.jsx
@@ -1,13 +1,15 @@
 
+import './style.css'
 function FooterItems(props) {
     return (
         <div>
 
-            <div className="">{item.title}</div>
+            <div className="">{props.title}</div>
             <div className="footer-item-content">
-                {item.content.map((content, index) => (
+                {props.content.map((content, index) => (
                     <div className="footer-item-content-item" key={index}>
                         {content}
+
                     </div>
                 ))}
             </div>
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,38 @@
 
 import FooterItems from "./FooterItems"
+import logoFooter from '../../assets/logoFooter.svg';
 function Footer(props) {
     return (
         <div>
-            <FooterItems title="Say Hello" items={['hello@easyType.ai']} />
+            <div className="footer">
+                <div className="box">
+                   <div className="logo">
+                    <img src={logoFooter} alt="" />
+                    <h1>Writing made Easy.</h1>
+                   </div>
+                <div className="content-box">
+                   <div className="item1">
+                   <FooterItems title="Say Hello" content={['hello@easyType.ai']} />
+                   </div>
+                   <div className="item2">
+                   <FooterItems title="Community" content={['facebook','instagram','Linkedin']} />
+                   </div>
+                   <div className="item3">
+                   <FooterItems title="Legal" content={['Privacy policy','Terms & conditions']} />
+                   </div>
+                </div>
+                <div className="search">
+                   <div className="text">
+                    <h3>Subscribe to the latest news</h3>
+                    </div>
+                    <div className="searchbar">
+                   <input type="text" placeholder="E-mail" name="search"></input>
+                   <button type="submit">SUBSCRIBE</button>
+                   </div>
+                   </div>
+            </div>
+            </div>
         </div>
     )
 }
-
 export default Footer
